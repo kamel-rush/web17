da6dd05422d4ef3cb4df3f5d4f47d5d1333c8bf4
kamel-rush <kamel@grandcircus.co>
2017-12-03 22:08:10 -0500

    add

---
+++ WebApplication17/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication17.Models;
+
+namespace WebApplication17.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = ConfigurationManager.AppSettings["key1"];
+
+
+            NorthwindEntities ORM = new NorthwindEntities();
+
+            ViewBag.Customers = ORM.Customers.ToList();
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
